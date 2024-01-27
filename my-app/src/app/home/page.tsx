107a206448ebc70b4930ecfe9239d0916c67e014
shweta-tamrakar <115773439+shweta-tamrakar@users.noreply.github.com>
2024-01-27 18:16:53 +0530

    lol

---
+++ my-app/src/app/home/page.tsx
@@ -0,0 +1,17 @@
+// import Link from 'next/link'
+// import React from 'react'
+ 
+// export default function page() {
+//   return (
+//     <>
+//   <Link href={`../`}>
+//     <button className='bg-gray-800'> home 2</button>
+//   </Link>
+//    </>
+//   )
+// }
+  {/* <Link href={`./home/`}>
+    <button className='bg-slate-50'>home 1</button>
+    <div className='lol'></div>
+    <div className='m-2 font-bold capitalize'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus, blanditiis perferendis. Dolor quis dolorem rem animi tempore. Harum doloremque, dicta quam, delectus illum distinctio ea quidem, quaerat ratione corrupti explicabo!</div>
+    </Link> */}
---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,34 @@
+// import Link from 'next/link'
+// import React from 'react'
+
+
+import React from 'react'
+
+export default function page() {
+  return (
+   <>
+    <div className='flex p-1 pl-6'>
+      <div className=''>
+        <p className='font-bold text-2xl m-4'>Lorem</p>
+      </div>
+      <div className='flex w-full justify-evenly items-center '>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+       
+       </div>
+       <div className='bg-black bg_img '></div>
+    </div>
+ <div className=' c1 '>
+  <h4>LANDING PAGE</h4>
+  <h5>DON'T TRY TO BE ORIGINAL</h5>
+  <h6>JUST TRY TO BE GOOD</h6>
+  <div className=' c2 flex justify-evenly'>
+    
+  </div>
+ </div>
+   </>
+   
+  )
+}
